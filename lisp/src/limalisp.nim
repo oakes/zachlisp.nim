edd8a9d566baaf45d5150ba4a8d57f616f1232b3
oakes <zach@sekao.net>
2022-11-13 17:19:19 -0500

    Add loop to main module

--- lisp/src/limalisp.nim
+++ lisp/src/limalisp.nim
@@ -1,2 +1,8 @@
+from limalisppkg/read import nil
+from limalisppkg/eval import nil
+
 when isMainModule:
-  echo "Hello, world!"
+  while true:
+    stdout.write "=> "
+    for cell in read.read(stdin.readLine):
+      echo eval.eval(cell)
